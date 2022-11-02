04965534d72c87a86e58d773d979d06c4e4d50a3
CoreDX1 <chrismaquis@gmail.com>
2022-11-02 20:49:43 -0300

    Funciona el db con los controladores

---
+++ FileExploreProject/Controllers/UsuariosController.cs
@@ -0,0 +1,26 @@
+﻿using FileExploreProject.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FileExploreProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuariosController : ControllerBase
+    {
+        private DbContextSqlite sqlite;
+
+        public UsuariosController(DbContextSqlite sqlite)
+        {
+            this.sqlite = sqlite;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var data = await sqlite.Usuarios.ToListAsync();
+            return Ok(data);
+        }
+    }
+}
